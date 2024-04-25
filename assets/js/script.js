06315e40ef1154be73c47d4feafd4cd2263b0dab
sjinko <sjinko@users.noreply.github.com>
2024-04-25 15:46:27 -0400

    #25 — Minimum viable navigation bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,14 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   });
 });
+
+// script.js
+
+function toggleMenu() {
+  var menu = document.querySelector('.menu');
+  if (menu.style.display === 'block') {
+      menu.style.display = 'none';
+  } else {
+      menu.style.display = 'block';
+  }
+}
